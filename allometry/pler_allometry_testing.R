995332accca98345fbb34ceb0868c56b57c936ef
ebel-c <cebel2@uoregon.edu>
2022-11-04 14:43:09 -0700

    testing pler allometric rel

--- allometry/pler_allometry_testing.R
+++ allometry/pler_allometry_testing.R
@@ -19,9 +19,11 @@ allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/
 
 ## Allometry data
 drought <- c(1, 3, 4, 6, 12, 14) ## create treatment vector
-pler_allo <- read.xlsx(paste0(allo_lead, "20220901_Allometry.xlsx"), sheet = 18) %>%
-  mutate(treatment = ifelse(block %in% drought, "D", "C"))
+#pler_allo <- read.xlsx(paste0(allo_lead, "20220901_Allometry.xlsx"), sheet = 18) %>%
+#  mutate(treatment = ifelse(block %in% drought, "D", "C"))
 
+pler_allo <- read.csv(paste0(allo_lead, "PLER_allometry-processing_20221101.csv"))
+  
 
 # Dat Range ####
 theme_set(theme_bw())
@@ -31,12 +33,14 @@ pler_dat <- all_dat_final %>%
 
 phyto<-ggplot(pler_dat, aes(x=inflor.g.rounded.percap)) +
   geom_histogram() +
-  facet_wrap(~treatment)
+  facet_wrap(~treatment) #+
+  #scale_x_continuous(breaks=seq(0,2.5,by=0.25))
 
 allo<-ggplot(pler_allo, aes(x=inflor.g)) +
   geom_histogram() +
   facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,2.5))
+  coord_cartesian(xlim = c(0,2.5)) #+
+  #scale_x_continuous(breaks=seq(0,2.5,by=0.25))
 
 
 ggarrange(phyto, allo, ncol = 1, nrow=2)
