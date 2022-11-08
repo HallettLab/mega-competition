0578d56619313f9818dc50e2573928d053539c92
Marina LaForgia <marina.laforgia@gmail.com>
2022-11-08 13:49:32 -0800

    updated allometry script to follow others

--- allometry/pler_allometry_testing.R
+++ allometry/pler_allometry_testing.R
@@ -11,39 +11,76 @@ calcSE<-function(x){
   sd(x2)/sqrt(length(x2))
 }
 
-# Read in Data ####
-## Processing data
-source("data_cleaning/merge_processing_collections_data.R")
-
-allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/" # Carmen's file path
+#Read in Data ####
+# specify dropbox pathway 
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/")){
+  # Carmen
+  allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/"
+  
+} else {
+  # Marina
+  allo_lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Allometry/Allometry_entered/"
+} 
 
 ## Allometry data
-drought <- c(1, 3, 4, 6, 12, 14) ## create treatment vector
-#pler_allo <- read.xlsx(paste0(allo_lead, "20220901_Allometry.xlsx"), sheet = 18) %>%
-#  mutate(treatment = ifelse(block %in% drought, "D", "C"))
-
 pler_allo <- read.csv(paste0(allo_lead, "PLER_allometry-processing_20221101.csv"))
-  
 
-# Dat Range ####
-theme_set(theme_bw())
+# Inflor-Seed Rel. ####
+## Visualize ####
+ggplot(pler_allo, aes(x=inflor.g, y=seed.num, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+## the error bars on the lines overlap for most of it - so probably not separate relationships here
 
-pler_dat <- all_dat_final %>%
-  filter(phyto == "PLER")
+ggplot(pler_allo, aes(x=inflor.g, y=seed.num)) +
+  geom_point() +
+  geom_smooth(method = "lm")
 
-phyto<-ggplot(pler_dat, aes(x=inflor.g.rounded.percap)) +
-  geom_histogram() +
-  facet_wrap(~treatment) #+
-  #scale_x_continuous(breaks=seq(0,2.5,by=0.25))
+ggplot(pler_allo, aes(x=inflor.g, y=seed.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ poly(x,2))
 
-allo<-ggplot(pler_allo, aes(x=inflor.g)) +
-  geom_histogram() +
-  facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,2.5)) #+
-  #scale_x_continuous(breaks=seq(0,2.5,by=0.25))
+ggplot(pler_allo[pler_allo$inflor.g > 0,], aes(x=inflor.g, y=seed.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ log(x))
 
+## Model ####
+inflorseeds <- lm(seed.num~inflor.g, data = pler_allo)
+summary(inflorseeds)
 
-ggarrange(phyto, allo, ncol = 1, nrow=2)
+inflorseeds2 <- lm(seed.num ~ inflor.g + I(inflor.g^2), data = pler_allo)
+summary(inflorseeds2)
 
-ggsave("allometry/preliminary_figs/allometric_relationship_fits/pler_allometry_check.png", height = 4, width = 6)
+# Biomass-Seed Rel. ####
+## Visualize ####
+ggplot(pler_allo, aes(x=total.biomass.g, y=seed.num, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+## the error bars on the lines overlap for most of it - so probably not separate relationships here
 
+ggplot(pler_allo, aes(x=total.biomass.g, y=seed.num)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+ggplot(pler_allo, aes(x=total.biomass.g, y=seed.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ poly(x,2))
+
+ggplot(pler_allo, aes(x=total.biomass.g, y=seed.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ log(x))
+
+## Model ####
+
+bioseeds <- lm(seed.num~total.biomass.g, data = pler_allo)
+summary(bioseeds)
+# y = 3.143 + 346.885x
+
+bioseeds2 <- lm(seed.num ~ total.biomass.g + I(total.biomass.g^2), data = pler_allo)
+summary(bioseeds2)
+
+## save the model outputs
+pler.allo.output <- bioseeds$coefficients
+
+# Clean Env ####
+rm(list = c("allo_lead", "pler_allo","inflorseeds", "inflorseeds2", "bioseeds2", "bioseeds"))
