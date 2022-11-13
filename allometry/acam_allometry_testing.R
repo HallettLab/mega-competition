ebed37dee747fc142e3a80b32642d82f7f2e9536
ebel-c <cebel2@uoregon.edu>
2022-11-13 08:50:20 -0800

    update acam allometry

--- allometry/acam_allometry_testing.R
+++ allometry/acam_allometry_testing.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggpubr)
+library(openxlsx)
 
 ## create a function to calculate standard error
 calcSE<-function(x){
@@ -9,23 +10,42 @@ calcSE<-function(x){
 
 
 # Read in Data ####
-## Processing data
-source("data_cleaning/merge_processing_collections_data.R")
+## Processing data ####
+source("data_cleaning/initial_data_prep/merge_processing_collections_data.R")
 
 acam_dat <- all_dat_final %>%
   filter(phyto == "ACAM")
 
-## Allometry data
-    ## none yet
+## Allometry data ####
+
+# specify dropbox pathway 
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/")){
+  # Carmen
+  allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/"
+  
+} else {
+  # Marina
+  allo_lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Allometry/Allometry_entered/"
+} 
+
+acam_allo <- read.xlsx(paste0(allo_lead, "20221113_Allometry-Processing.xlsx"), sheet = 1) %>%
+  filter(!is.na(flower.num))
 
 
 # Flower Dat Range ####
-ggplot(acam_dat, aes(x=total.biomass.rounded.percap)) +
+theme_set(theme_bw())
+
+phyto<-ggplot(acam_dat, aes(x=total.biomass.rounded.percap)) +
   geom_histogram() +
-  facet_wrap(~treatment)
+  facet_wrap(~treatment) +
+  coord_cartesian(xlim = c(0,20))
 
-ggsave("acam_dat_range.png", width = 5, height = 3)
+allo<-ggplot(acam_allo, aes(x=total.biomass.g)) +
+  geom_histogram() +
+  facet_wrap(~treatment) +
+  coord_cartesian(xlim = c(0,20))
 
 
+ggarrange(phyto, allo, ncol = 1, nrow=2)
 
-
+ggsave("allometry/preliminary_figs/allometric_relationship_fits/acam_allometry_check.png", height = 4, width = 6)
