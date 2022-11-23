b72463e6df1664ad3200323aa9e136f28a94406c
ebel-c <cebel2@uoregon.edu>
2022-11-23 10:16:33 -0800

    prelim bg seeds calcs

--- data_cleaning/bkgrd-processing_data-cleaning/bkgrd_calculations.R
+++ data_cleaning/bkgrd-processing_data-cleaning/bkgrd_calculations.R
@@ -8,9 +8,26 @@ source("data_cleaning/bkgrd-processing_data-cleaning/bkgrd-processing_data-clean
 ## germination data
 source("data_cleaning/bkgrd-processing_data-cleaning/germination_rates.R")
 
+## collections data (for bkgrd.n.indiv)
+source("data_cleaning/phyto-collections_data-cleaning/phyto-collections_data-cleaning.R")
 
+
+bkgrd.n.indiv <- collectionsC %>%
+  select(unique.ID, block, plot, bkgrd, bkgrd.n.indiv)
+
+bkgrd.test <- left_join(bkgrd.n.indiv, bg.germ, by = "bkgrd")
+bkgrd.test2 <- left_join(bkgrd.test, bg.seeds, by = c("block", "plot", "bkgrd"))
+
+bkgrd.seeds <- bkgrd.test2 %>%
+  mutate(bg.seeds.in = bkgrd.n.indiv/avg.germ,
+         bg.seeds.out = bkgrd.n.indiv*bg.avg.seed.num) %>%
+  select(unique.ID, bg.seeds.in, bg.seeds.out, bkgrd)
 
 ## calculate background seeds in
+    ## calculate by: seeds.in * germination.rate = stems.out
+    ## we will use ONLY the wet germination rates for these calculations as we did not apply our drought treatment until after germinating rains
+
 ## calculate background seeds out
+    ## avg seed per indiv * stems out = seeds.out
 
 
--- data_cleaning/bkgrd-processing_data-cleaning/germination_rates.R
+++ data_cleaning/bkgrd-processing_data-cleaning/germination_rates.R
@@ -8,6 +8,8 @@ calcSE<-function(x){
 
 # Load Libraries ####
 library(tidyverse)
+library(stringr)
+
 
 # Load Data ####
 if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Processing/Background-Processing/Background-Processing_entered/")){
@@ -21,28 +23,84 @@ if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Dat
 
 germ <- read.csv(paste0(lead, "20220218_Germination-Data_full.csv"))
 
+# Change sp codes ####
+germ2 <- germ %>%
+  mutate(species2 = sub("^(.{1,5}).(.*)", "\\1\\2", Species),
+         species = sub("^(.{1,2}).(.*)", "\\1\\2", species2)) %>%
+  select(-Species, -species2)
+
+
   
-# Explore Data ####
-
+# Calc Avg Germ ####
 #Calculate average germination rates per species per treatment
-germ.sum.trt <- germ %>%
-  group_by(Species, Temp, WP) %>%
+germ.sum.trt <- germ2 %>%
+  group_by(species, Temp, WP) %>%
   summarize(avg.germ = mean(p.germ), se.germ = calcSE(p.germ))
-
 
 #Calculate average germination rates per species
-germ.sum.sp <- germ %>%
-  group_by(Species) %>%
-  summarize(avg.germ = mean(p.germ), se.germ = calcSE(p.germ))
-
-germ.sum.sp$Temp <- 15 # "average temp"
-germ.sum.sp$WP <- -0.25 # "average water potential"
+germ.sum.sp <- germ2 %>%
+  group_by(species) %>%
+  summarize(avg.germ = mean(p.germ), se.germ = calcSE(p.germ)) %>%
+  mutate(Temp = 15, ## "average temp"
+         WP = -0.25) ## "average water potential"
 
 germ.sum.sp <- germ.sum.sp[,c(1,4,5,2,3)]
 
 germ.sum <- rbind(germ.sum.sp, germ.sum.trt)
 
+
+# Explore Data ####
 ggplot(germ.sum.trt[germ.sum.trt$WP != -0.5,], aes(x = as.factor(Temp), y = avg.germ)) +
   geom_point() +
   geom_errorbar(aes(ymin = avg.germ - se.germ, ymax = avg.germ + se.germ, width = 0.1)) +
   facet_wrap(~Species)
+
+
+# Create BG germ DF ####
+## Germ by temperature
+avgtemp <- c("ACAM", "AMME", "AVBA", "BRHO", "CESO", "TACA", "LOMU", "GITR", "LENI", "MAEL", "MICA", "PLNO", "TWIL", "THIR")
+## no effect of temp on germination, use average
+
+vartemp <- c("ANAR", "BRNI", "CLPU")
+## variable temp: for blocks seeded in warm rain use warm germ, otherwise cold
+
+coldtemp <- c("PLER")
+## better germ in cold temps, use cold
+
+species <- unique(germ.sum$species)
+
+## create empty data frame
+df <- data.frame()
+
+for (i in 1:length(species)) {
+
+sp <- species[i]
+
+if (sp %in% avgtemp) {
+  
+  temp <- germ.sum %>%
+    filter(Temp == 15, species == sp)
+  
+} else if (sp %in% vartemp) {
+  
+  temp <- germ.sum %>%
+    filter(WP == -0.5, species == sp)
+  
+} else {
+  
+  temp <- germ.sum %>%
+    filter(species == sp, Temp == 10, WP == -0.5)
+  
+}
+  
+df <- rbind(df, temp)
+
+}
+
+bg.germ <- df %>%
+  mutate(bkgrd = species)
+
+
+
+
+rm(list = c("germ.sum.sp", "germ.sum.trt", "germ", "df"))
