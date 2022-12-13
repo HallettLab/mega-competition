78bca84a3f9a178fab6ce10e292525f9ea77712b
Marina LaForgia <marina.laforgia@gmail.com>
2022-12-13 15:12:37 -0800

    Allometry testing

--- allometry/acam_allometry_testing.R
+++ allometry/acam_allometry_testing.R
@@ -1,3 +1,12 @@
+## ACAM Allometric Relationship
+## this script 
+    ## 1. checks that phyto & allometry data cover approx the same range
+          ## after this is checked & confirmed to be okay, comment out this part so that we do not load & reload the same phyto data multiple times in later scripts.
+    ## 2. tests & plots various allometric relationships
+    ## 3. saves the output from the final best model for use later in predicting seed output.
+
+rm(list=ls())
+
 library(tidyverse)
 library(ggpubr)
 library(openxlsx)
@@ -9,15 +18,6 @@ calcSE<-function(x){
 }
 
 
-# Read in Data ####
-## Processing data ####
-source("data_cleaning/phyto-processing_data-cleaning/basic-cleaning_all-phytos.R")
-
-#acam_dat <- all_dat_final %>%
- # filter(phyto == "ACAM")
-
-## Allometry data ####
-
 # specify dropbox pathway 
 if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/")){
   # Carmen
@@ -28,36 +28,78 @@ if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Dat
   allo_lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Allometry/Allometry_entered/"
 } 
 
-acam_allo <- read.csv(paste0(allo_lead, "Allometry-Processing_ACAM-flowers_20221202.csv")) %>%
-  filter(!is.na(flower.num))
+acam_flower_allo <- read.csv(paste0(allo_lead, "ACAM-flowers_allometry-processing_20221213.csv"))
 
-acamC2 <- acamC %>%
-  mutate(total.biomass.g.percap = total.biomass.g/phyto.n.indiv)
+acam_seed_allo <- read.csv(paste0(allo_lead, "ACAM-seeds_allometry-processing_20221213.csv"))
 
-
-# Flower Dat Range ####
-theme_set(theme_bw())
-
-phyto<-ggplot(acamC2, aes(x=total.biomass.g.percap)) +
-  geom_histogram() +
-  facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,20))
-
-allo<-ggplot(acam_allo, aes(x=total.biomass.g)) +
-  geom_histogram() +
-  facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,20))
-
-
-ggarrange(phyto, allo, ncol = 1, nrow=2)
-
-ggsave("allometry/preliminary_figs/allometric_relationship_fits/acam_allometry_check.png", height = 4, width = 6)
-
-
-ggplot(acam_allo, aes(x=total.biomass.g, y=flower.num, color = treatment)) +
+ggplot(acam_flower_allo, aes(x=total.biomass.g, y=flower.num)) +
   geom_point() +
   geom_smooth(method = lm)
 
+# Seed Distrib ####
+ggplot(acam_seed_allo, aes(x=seed.num)) +
+  geom_histogram()
+
+## separate by treatment
+ggplot(acam_seed_allo, aes(x=seed.num)) +
+  geom_histogram()+
+  facet_wrap(~treatment)
+
+## look at sample num per treatment
+nrow(acam_seed_allo[acam_seed_allo$treatment == "D",]) ## 36
+nrow(acam_seed_allo[acam_seed_allo$treatment == "C",]) ## 36
+
+
+# Calc Seeds/Flower ####
+## calc overall mean
+acam_mean_seeds <- mean(acam_seed_allo$seed.num, na.rm = T)
+acam_mean_seeds ## 1.29
+
+## calc mean by treatment
+acam_seed_means <- acam_seed_allo %>%
+  group_by(treatment) %>%
+  summarise(mean_seeds = mean(seed.num, na.rm = T), SE_seeds = calcSE(seed.num))
+
+## plot this
+ggplot(acam_seed_means, aes(x=treatment, y = mean_seeds)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_seeds - SE_seeds, ymax = mean_seeds + SE_seeds), width = 0.25) +
+  ylab("Mean Seeds per Flower") + xlab ("Treatment")
+
+## use an anova to test signif differences b/w categories
+seedtrt <- aov(seed.num ~ treatment, data = acam_seed_allo)
+
+summary(seedtrt)
+TukeyHSD(seedtrt) # they differ but this seems to be because of empty pods
+
+# TotBio - Flower Rel. ####
+## Combine drought and controls together for biomass-flower relationship
+
+## visualize ####
+ggplot(acam_flower_allo, aes(x=total.biomass.g, y=flower.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, size = 0.75, formula = y ~ x)
+
+## plot as polynomial
+ggplot(acam_flower_allo, aes(x=total.biomass.g, y=flower.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, size = 0.75, formula = y ~ poly(x, 2))
+
+
+## model ####
+## test linear model first
+acam_fallo_rel <- lm(flower.num ~ total.biomass.g, data = acam_flower_allo)
+summary(acam_fallo_rel) # r2 = 0.962
+
+## test polynomial model
+# acam_fallo_rel <- lm(flower.num ~ total.biomass.g + I(total.biomass.g^2), data = acam_flower_allo)
+# summary(acam_fallo_rel) # r2 = 0.963
+
+
+## save the model outputs
+acam.allo.output <- acam_fallo_rel$coefficients
+
+rm(list = c("allo_lead", "acam_fallo_rel", "acam_flower_allo", "acam_mean_seeds",  "acam_seed_allo", "seedtrt"))
 
 
 
--- allometry/anar_allometry_testing.R
+++ allometry/anar_allometry_testing.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(ggpubr)
 library(openxlsx)
 
+rm(list=ls())
 
 ## create a function to calculate standard error
 calcSE<-function(x){
@@ -11,11 +12,6 @@ calcSE<-function(x){
 
 
 # Read in Data ####
-## Processing data
-source("data_cleaning/initial_data_prep/merge_processing_collections_data.R")
-
-anar_dat <- all_dat_final %>%
-  filter(phyto == "ANAR")
 
 ## Allometry data
 # specify dropbox pathway 
@@ -29,25 +25,84 @@ if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Dat
 } 
 
 
-anar_allo <- read.xlsx(paste0(allo_lead, "20221113_Allometry-Processing.xlsx"), sheet = 5) %>%
-  filter(!is.na(flower.num))
+anar_allo <- read.csv(paste0(allo_lead, "ANAR-flowers_allometry-processing_20221213.csv")) 
 
-anar_seeds <- read.xlsx(paste0(allo_lead, "20221113_Allometry-Processing.xlsx"), sheet = 6)
+anar_seeds <- read.csv(paste0(allo_lead, "ANAR-seeds_allometry-processing_20221213.csv"))
 
-# Flower Dat Range ####
-theme_set(theme_bw())
+# Seed Distrib ####
+ggplot(anar_seeds, aes(x=seed.num)) +
+  geom_histogram()
 
-phyto<-ggplot(anar_dat, aes(x=total.biomass.rounded.percap)) +
-  geom_histogram() +
-  facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,6))
+## separate by treatment
+ggplot(anar_seeds, aes(x=seed.num)) +
+  geom_histogram()+
+  facet_wrap(~Treatment)
 
-allo<-ggplot(anar_allo, aes(x=total.biomass.g)) +
-  geom_histogram() +
-  facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,6))
+## look at sample num per treatment
+nrow(anar_seeds[anar_seeds$Treatment == "D",]) ## 39
+nrow(anar_seeds[anar_seeds$Treatment == "C",]) ## 46
 
 
-ggarrange(phyto, allo, ncol = 1, nrow=2)
+# Calc Seeds/Flower ####
+## calc overall mean
+anar_mean_seeds <- mean(anar_seeds$seed.num, na.rm = T)
+anar_mean_seeds ## 12.34
 
-ggsave("allometry/preliminary_figs/allometric_relationship_fits/anar_allometry_check.png", height = 4, width = 6)
+## calc mean by treatment
+anar_seed_means <- anar_seeds %>%
+  group_by(Treatment) %>%
+  summarise(mean_seeds = mean(seed.num, na.rm = T), SE_seeds = calcSE(seed.num))
+
+## plot this
+ggplot(anar_seed_means, aes(x=Treatment, y = mean_seeds)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_seeds - SE_seeds, ymax = mean_seeds + SE_seeds), width = 0.25) +
+  ylab("Mean Seeds per Flower") + xlab ("Treatment")
+
+## use an anova to test signif differences b/w categories
+seedtrt <- aov(seed.num ~ Treatment, data = anar_seeds)
+
+summary(seedtrt)
+TukeyHSD(seedtrt) # no sig diff, use mean seeds
+
+# TotBio - Flower Rel. ####
+## Combine drought and controls together for biomass-flower relationship
+
+## visualize ####
+ggplot(anar_allo, aes(x = total.biomass.g, y = flower.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, size = 0.75, formula = y ~ x)
+
+# ggplot(anar_allo, aes(x = total.biomass.g, y = flower.num, col = treatment)) +
+#   geom_point() +
+#   geom_smooth(method = "lm", alpha = 0.25, size = 0.75, formula = y ~ x)
+
+## plot as polynomial
+ggplot(anar_allo, aes(x = total.biomass.g, y = flower.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, size = 0.75, formula = y ~ poly(x, 2))
+
+
+## model ####
+## test linear model first
+# anar_fallo_rel <- lm(flower.num ~ total.biomass.g, data = anar_allo)
+# summary(anar_fallo_rel) # r2 = 0.931
+
+## test polynomial model
+anar_fallo_rel <- lm(flower.num ~ total.biomass.g + I(total.biomass.g^2), data = anar_allo)
+summary(anar_fallo_rel) # r2 = 0.934
+
+# Test without outlier
+# anar_fallo_rel <- lm(flower.num ~ total.biomass.g, data = anar_allo[anar_allo$total.biomass.g<5,])
+# summary(anar_fallo_rel) # r2 = 0.774
+# 
+# 
+# anar_fallo_rel <- lm(flower.num ~ total.biomass.g + I(total.biomass.g^2), data = anar_allo[anar_allo$total.biomass.g<5,])
+# summary(anar_fallo_rel) # r2 = 0.829
+# polynomial is better with and without outlier
+
+## save the model outputs
+anar.allo.output <- anar_fallo_rel$coefficients
+
+rm(list = c("allo_lead", "anar_fallo_rel", "anar_allo",  "anar_seeds", "anar_seed_means", "anar_seed_allo", "seedtrt"))
+
