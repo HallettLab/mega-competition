e6379e93af26ab8132f61dce06a7069583ddaea8
ebel-c <cebel2@uoregon.edu>
2022-12-16 21:38:56 -0800

    compile phyto seeds in/out!

--- data_cleaning/phyto-processing_data-cleaning/compile_phyto-processing_data.R
+++ data_cleaning/phyto-processing_data-cleaning/compile_phyto-processing_data.R
@@ -6,9 +6,11 @@ source("data_cleaning/phyto-processing_data-cleaning/BRHO_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/GITR_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/AVBA_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/PLER_phyto.R")
-
+source("data_cleaning/phyto-processing_data-cleaning/ACAM_phyto.R")
+source("data_cleaning/phyto-processing_data-cleaning/ANAR_phyto.R")
+source("data_cleaning/phyto-processing_data-cleaning/MAEL_phyto.R")
 
 ## the final data frames are brho.phyto and gitr.phyto
-all.phytos <- do.call("rbind", list(brho.phyto, gitr.phyto, pler.phyto, avba.phyto))
+all.phytos <- do.call("rbind", list(brho.phyto, gitr.phyto, pler.phyto, avba.phyto, acam.phyto, anar.phyto, mael.phyto))
     ## add more phytos in as they are ready here.
   
