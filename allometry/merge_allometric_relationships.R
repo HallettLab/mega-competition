693381f729a9fcc43cc0f7fc827b649851da4283
ebel-c <cebel2@uoregon.edu>
2022-12-16 16:11:41 -0800

    update allo merge

--- allometry/merge_allometric_relationships.R
+++ allometry/merge_allometric_relationships.R
@@ -15,34 +15,12 @@
 source("allometry/gitr_allometry_testing.R")
 source("allometry/brho_allometry_testing.R")
 source("allometry/pler_allometry_testing.R")
+source("allometry/mael_allometry_testing.R")
+source("allometry/acam_allometry_testing.R")
+source("allometry/anar_allometry_testing.R")
 
-# Modify Here w/New Sp ####
-## make a vector of all completed relationship names
-allo.outputs <- c("GITR.allo.output", "BRHO.allo.output", "PLER.allo.output")
+## merge all together
+allo.df <- do.call("rbind", list(ACAM.allo.output, ANAR.allo.output, BRHO.allo.output, GITR.allo.output, MAEL.allo.output, PLER.allo.output))
 
-# Merge Model Outputs ####
-allo.df <- as.data.frame(matrix(ncol = 4))
-colnames(allo.df) <- c("species", "intercept", "coeff", "coeff2")
-
-for (i in 1:length(allo.outputs)) {
-  
-  sp <- get(allo.outputs[i]) ## fetch the correct model
-  
-  temp <- data.frame(species = NA, intercept = NA, coeff = NA, coeff2 = NA) ## set colnames in temp dataframe
-  
-  temp$species <- substr(allo.outputs[i], 1, 4) ## get sp name from first 4 letters of the model output object name
-  
-  ## add in model outputs
-  temp$intercept <- sp[1] ## intercept
-  temp$coeff <- sp[2] ## slope
-  temp <- temp %>%
-    mutate(coeff2 = ifelse(!is.na(sp[3]), sp[3], 0)) ## put in a 0 rather than NA if there is no 2nd model coeff
-  
-  ## append
-  allo.df <- rbind(allo.df, temp)
-  
-}
-
-
-allo.df <- allo.df %>%
-  filter(!is.na(species))
+## clean up env
+rm(list = c("ACAM.allo.output", "ANAR.allo.output", "BRHO.allo.output", "GITR.allo.output", "MAEL.allo.output", "PLER.allo.output"))
