1147294ed0d909f8ce45337f50bade24e77b10e5
ebel-c <cebel2@uoregon.edu>
2022-12-16 17:39:11 -0800

    ANAR seeds in/out calc

--- data_cleaning/phyto-processing_data-cleaning/ANAR_phyto.R
+++ data_cleaning/phyto-processing_data-cleaning/ANAR_phyto.R
@@ -53,5 +53,32 @@ for(i in colnames(anar_final)[13:14]) {
 }
 unique(df$census.notes)
 
-## 12-23-18 is showing up twice on the notes df. Not sure why.
+## 12-23-18 is showing up twice on the notes df. Not sure why. Oh probably shows up in both for loops
 ## lots of notes but all thankfully show phyto # changes!! 
+
+# Make Phyto DF ####
+anar.phyto <- anar_final %>%
+  mutate(ANAR.flowers.out = (allo.df[allo.df$Species == "ANAR",2] + ## intercept
+                               (allo.df[allo.df$Species == "ANAR",5]*total.biomass.g)), ## slope
+         ## use tot.bio to flower.num to get flowers out
+         
+         ANAR.flowers.out = ifelse(total.biomass.g > 5, 419, ANAR.flowers.out), ## for the largest sample, put in flower # manually as this didn't work well with the allometric relationship
+         
+         
+         phyto.seed.out = ifelse(treatment == "D",  allo.df[allo.df$Species == "ANAR",13]*ANAR.flowers.out,  allo.df[allo.df$Species == "ANAR",11]*ANAR.flowers.out),
+         ## use avg seed num per trt to calculate seeds out
+         
+         phyto.seed.in = ifelse(!is.na(phyto.unique), phyto.n.indiv, 3),
+         ## for phyto uniques, use the # indiv as the seeds.in, otherwise put 3 as the default
+         
+         phyto.seed.in = ifelse(phyto.n.indiv > 3, phyto.n.indiv, phyto.seed.in)) %>%
+  ## then, check for # indiv > 3, use # indiv as seeds.in here also
+  select(unique.ID, phyto, phyto.n.indiv, phyto.seed.in, phyto.seed.out)
+
+## still need lots of phyto.seed.in updates likely
+
+
+ggplot(anar.phyto, aes(x=phyto.seed.out)) +
+  geom_histogram()
+
+rm(list = c("anar_final", "anar_int", "anarC"))
