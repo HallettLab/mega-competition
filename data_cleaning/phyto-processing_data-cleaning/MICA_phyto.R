b72982d943b2c0f50c0b4ea57f1a75afdf7d93e5
ebel-c <cebel2@uoregon.edu>
2023-01-06 16:47:56 -0800

    start cleaning MICA phyto processing

---
+++ data_cleaning/phyto-processing_data-cleaning/MICA_phyto.R
@@ -0,0 +1,39 @@
+# Load packages ####
+library(tidyverse)
+
+# Read in Data ####
+## phyto-processing data
+## specify dropbox pathway 
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Processing/Phytometer-Processing/Phytometer-Processing_entered/")){
+  # Carmen
+  lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Processing/Phytometer-Processing/Phytometer-Processing_entered/"
+  
+} else {
+  # Marina
+  lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Processing/Phytometer-Processing/Phytometer-Processing_entered/"
+}
+
+mica <- read.csv(paste0(lead, "MICA_phyto-processing_20221018.csv"))
+
+## basic cleaning function
+source("data_cleaning/phyto-processing_data-cleaning/basic_cleaning_function.R")
+
+## allometry data
+source("allometry/merge_allometric_relationships.R")
+
+## uniqueID key
+source("data_cleaning/unique_key.R")
+
+
+# Basic Cleaning ####
+micaC <- basic_cleaning_func(mica)
+
+## need to add in unique.IDs here
+mica_int <- left_join(micaC, unique.key, by = c("treatment", "block", "plot", "sub", "bkgrd", "dens", "phyto", "phyto.unique")) %>%
+  mutate(unique.ID = unique.ID.y)
+
+## Follow up on spot checks ####
+mica_int[mica_int$unique.ID == 8028,]
+## 12-31-8
+mica_int[mica_int$unique.ID == 2042,]
+## 3-41-24
