fcebb8ae0be46157ef5dccb32072b92d05a8cb28
Marina LaForgia <marina.laforgia@gmail.com>
2023-01-25 08:57:38 -0800

    started allometry for taca and thir

---
+++ allometry/taca_allometry_testing.R
@@ -0,0 +1,74 @@
+## TACA Allometric Relationship
+## this script 
+    ## 1. checks that phyto & allometry data cover approx the same range
+          ## after this is checked & confirmed to be okay, comment out this part so that we do not load & reload the same phyto data multiple times in later scripts.
+    ## 2. tests & plots various allometric relationships
+    ## 3. saves the output from the final best model for use later in predicting seed output.
+
+library(tidyverse)
+library(ggpubr)
+library(openxlsx)
+
+## create a function to calculate standard error
+calcSE<-function(x){
+  x2<-na.omit(x)
+  sd(x2)/sqrt(length(x2))
+}
+
+# specify dropbox pathway 
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/")){
+  # Carmen
+  allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/"
+  
+} else {
+  # Marina
+  allo_lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Allometry/Allometry_entered/"
+} 
+
+## Allometry data
+taca_allo <- read.csv(paste0(allo_lead, "TACA_allometry-processing_20230116.csv"))
+
+nrow(taca_allo[taca_allo$treatment == "D",]) ## 20
+nrow(taca_allo[taca_allo$treatment == "C",]) ## 20
+
+ggplot(taca_allo, aes(x = total.biomass.g, y = seeds.num, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+## use an anova to test signif differences b/w categories
+seedtrt <- aov(seeds.num ~ treatment, data = taca_allo)
+
+summary(seedtrt)
+TukeyHSD(seedtrt) # no difference between treatments
+
+## Phyto data
+taca_phyto <- read.csv(paste0(allo_lead, "TACA_phyto-processing_20221108.csv"))
+
+
+## visualize ####
+ggplot(taca_allo, aes(x = total.biomass.g, y = seeds.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, linewidth = 0.75, formula = y ~ x)
+
+## plot as polynomial
+ggplot(taca_allo[taca_allo$total.biomass.g<20,], aes(x = total.biomass.g, y = seeds.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, linewidth = 0.75, formula = y ~ poly(x, 2))
+
+## model ####
+## test linear model first
+taca_allo_rel <- lm(seeds.num ~ total.biomass.g, data = taca_allo)
+summary(taca_allo_rel) # r2 = 0.9731
+
+## test polynomial model
+taca_allo_rel <- lm(seeds.num ~ total.biomass.g + I(total.biomass.g^2), data = taca_allo)
+summary(taca_allo_rel) # r2 = 0.9973
+
+# what about without outliers?
+## test linear model first
+taca_allo_rel <- lm(seeds.num ~ total.biomass.g, data = taca_allo[taca_allo$total.biomass.g<20,])
+summary(taca_allo_rel) # r2 = 0.957
+
+## test polynomial model
+taca_allo_rel <- lm(seeds.num ~ total.biomass.g + I(total.biomass.g^2), data = taca_allo[taca_allo$total.biomass.g<20,])
+summary(taca_allo_rel) # r2 = 0.9668
--- allometry/thir_allometry_testing.R
+++ allometry/thir_allometry_testing.R
@@ -8,20 +8,3 @@ calcSE<-function(x){
 }
 
 
-# Read in Data ####
-## Processing data
-source("data_cleaning/phyto-processing_data-cleaning/basic-cleaning_all-phytos.R")
-
-thir_dat <- all_dat_final %>%
-  filter(phyto == "THIR-I")
-
-## Allometry data
-## none yet
-
-
-# Flower Dat Range ####
-ggplot(thir_dat, aes(x=total.biomass.rounded.percap)) +
-  geom_histogram() +
-  facet_wrap(~treatment)
-
-ggsave("thir_dat_range.png", width = 5, height = 3)
