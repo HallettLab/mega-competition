49191ffd6e92ea7dee7cf130fdd55ed130c142a8
ebel-c <cebel2@uoregon.edu>
2023-02-16 09:35:44 -0800

    more percap exploration

--- Models/per-capita-GR/traits_percapita_GR.R
+++ Models/per-capita-GR/traits_percapita_GR.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(stats)
 
 
 # Read in Data ####
@@ -22,14 +23,23 @@ traits <- read.csv(paste0(lead, "Megacomp_adult-traits.csv")) %>%
 
 percap.with.traits <- left_join(percap.growth, traits, by = "phyto") %>%
   mutate(phyto.nativity = nativity,
-         phyto.growth.form = growth_form) %>%
-  select(1:14, 40:41)
+         phyto.growth.form = growth_form,
+         phyto.height.cm = Height_cm,
+         phyto.sla.cm2.g = SLA_cm2.g,
+         phyto.lwc = LWC, 
+         phyto.cn = CN,
+         bkgrd = bkgrd.x) %>%
+  select(unique.ID:sub, dens:dens.num, phyto.nativity:bkgrd)
 
 percap.with.bg.traits <- left_join(percap.with.traits, traits, by = "bkgrd") %>%
   mutate(bkgrd.nativity = nativity,
          bkgrd.growth.form = growth_form, 
+         bkgrd.height.cm = Height_cm,
+         bkgrd.sla.cm.g = SLA_cm2.g,
+         bkgrd.lwc = LWC,
+         bkgrd.cn = CN,
          phyto = phyto.x) %>%
-  select(1:16, 43:44) %>%
+  select(unique.ID, phyto.n.indiv:bkgrd,bkgrd.nativity:phyto) %>%
   mutate(bkgrd.group = paste(bkgrd.nativity, bkgrd.growth.form, sep = "_"),
          phyto.group = paste(phyto.nativity, phyto.growth.form, sep = "_"), 
          log.percap = log(percap.growthrate))
@@ -47,10 +57,7 @@ ggplot(percap.with.bg.traits, aes(x=dens, y=log.percap, by = phyto.group, fill =
   facet_wrap(~bkgrd.group) +
   geom_hline(yintercept = 0, linetype = "dashed")
 
-ggsave("models/per-capita-GR/log.per.cap.functional.groups.png", width = 8, height = 4)
-
-
-
+#ggsave("models/per-capita-GR/log.per.cap.functional.groups.png", width = 8, height = 4)
 
 
 
