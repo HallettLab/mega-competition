d4839dfee242358a86deac5241d943d432f45813
ebel-c <cebel2@uoregon.edu>
2023-03-16 14:59:21 -0400

    find brni & leni allo rel

--- allometry/brni_allometry_testing.R
+++ allometry/brni_allometry_testing.R
@@ -1,4 +1,6 @@
+## BRNI Allometry Testing 
 
+## set up env
 library(tidyverse)
 theme_set(theme_bw())
 
@@ -7,6 +9,7 @@ calcSE<-function(x){
   x2<-na.omit(x)
   sd(x2)/sqrt(length(x2))
 }
+
 
 # Read in Data ####
 # specify dropbox pathway 
@@ -20,42 +23,223 @@ if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Dat
 } 
 
 ## Allometry data
-brni_allo <- read.csv(paste0(allo_lead, "BRNI_allometry-processing_20230301.csv"))
+brni_allo <- read.csv(paste0(allo_lead, "BRNI_allometry-processing_20230315.csv"))
 
-brni_seeds <- read.csv(paste0(allo_lead, "BRNI-seeds_allometry-processing_20230223.csv"))
-
-
-brni_seed_means <- brni_seeds %>%
-  group_by(treatment) %>%
-  summarise(mean_seeds = mean(seeds.per.one.pod, na.rm = T), se_seeds = calcSE(seeds.per.one.pod))
+brni_seeds <- read.csv(paste0(allo_lead, "BRNI-seeds_allometry-processing_20230315.csv"))
 
 
+## phyto-processing data
+## specify dropbox pathway 
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Processing/Phytometer-Processing/Phytometer-Processing_entered/")){
+  # Carmen
+  lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Processing/Phytometer-Processing/Phytometer-Processing_entered/"
+  
+} else {
+  # Marina
+  lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Processing/Phytometer-Processing/Phytometer-Processing_entered/"
+} 
+
+brni.phyto <- read.csv(paste0(lead, "BRNI_phyto-processing_20230315.csv"))
+
+
+
+
+# Clean ####
 brni_allo2 <- brni_allo %>%
-  filter(!is.na(viable.pod.num)) %>%
-  mutate(seeds.predicted = viable.pod.num*brni_seed_means[brni_seed_means$treatment == "C",]$mean_seeds, 
-         viability = viable.pod.num/pod.num)
+  mutate(total.flower.num = pod.num + flower.num, 
+         prop.viable = viable.pod.num/pod.num)
 
-brni_allo2$seeds.num <- as.numeric(brni_allo2$seeds.num)
+## merge with phyto data for exploration with viability
+brni.allo.phyto <- left_join(brni_allo2, brni.phyto, by = c("block", "plot", "sub", "phyto.unique"))
 
-ggplot(brni_allo2, aes(x=seeds.num, y=seeds.predicted)) +
+
+# Bio-Flower Relationship ####
+## Visualize ####
+### linear ####
+ggplot(brni_allo2, aes(x=total.biomass.g, y=total.flower.num)) +
   geom_point() +
-  geom_abline(slope = 1)
+  geom_smooth(method = "lm")
 
-ggplot(brni_allo2, aes(x=total.biomass.g, viable.pod.num)) +
-  geom_point()+
-  ggtitle("BRNI Allo")
-ggsave("allometry/preliminary_figs/brni_bio_via_pods.png", width = 5, height = 3)
-
-ggplot(brni_allo2, aes(x=total.biomass.g, pod.num)) +
+ggplot(brni_allo2, aes(x=total.biomass.g, y=viable.pod.num)) +
   geom_point() +
-  ggtitle("BRNI Allo")
-ggsave("allometry/preliminary_figs/brni_bio_all_pods.png", width = 5, height = 3)
+  geom_smooth(method = "lm")
 
-ggplot(brni_allo2, aes(x=total.biomass.g, seeds.num)) +
+ggplot(brni_allo2, aes(x=total.biomass.g, y=total.flower.num, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+## the two treatments have pretty different slopes, the D treatment appears largely driven by one large sample though.
+
+### polynomial ####
+ggplot(brni_allo2, aes(x = total.biomass.g, y = total.flower.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, linewidth = 0.75, formula = y ~ poly(x, 2)) +
+  ggtitle("BRNI poly")
+#ggsave("allometry/preliminary_figs/TWIL_flowers_poly.png", width = 4, height = 2.5)
+
+ggplot(brni_allo2, aes(x = total.biomass.g, y = total.flower.num, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, linewidth = 0.75, formula = y ~ poly(x, 2))
+
+## Model ####
+### *linear ####
+brni_allo_lin <- lm(total.flower.num~total.biomass.g, data = brni_allo2)
+summary(brni_allo_lin)
+## r2 = 0.9425
+
+
+### poly ####
+brni_allo_pol <- lm(total.flower.num ~ total.biomass.g + I(total.biomass.g^2) + treatment, data = brni_allo2)
+summary(brni_allo_pol) 
+# r2 = 0.9426
+## no signif effect of the poly treatment, so use linear
+
+### lin with trt ####
+brni_allo_lin_trt <- lm(total.flower.num~total.biomass.g+treatment, data = brni_allo2)
+summary(brni_allo_lin_trt)
+## r2 = 0.9492
+
+
+
+# Viability ####
+## visually explore ####
+
+## check distribution
+ggplot(brni_allo2, aes(x=prop.viable)) +
+  geom_histogram()
+
+## distribution by treatment
+ggplot(brni_allo2, aes(x=prop.viable)) +
+  geom_histogram() +
+  facet_wrap(~treatment)
+## control distribution has a peak closer to 0.25; drought distribution is fairly even all the way across...
+
+
+## viability prop by bio
+ggplot(brni_allo2, aes(x=total.biomass.g, y=prop.viable)) +
+  geom_point()
+
+## viability prop by bio by treatment
+ggplot(brni_allo2, aes(x=total.biomass.g, y=prop.viable, color = treatment)) +
+  geom_point()
+
+## viability prop by block
+ggplot(brni_allo2, aes(x=block, y=prop.viable)) +
   geom_point()
 
 
-ggplot(brni_allo2, aes(x=total.biomass.g, y=viability)) +
-  geom_point()
-ggsave("allometry/preliminary_figs/brni_bio_viability.png", width = 5, height = 3)
+## viability prop by total flowers
+ggplot(brni_allo2, aes(x=total.flower.num, y=prop.viable)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  coord_cartesian(ylim = c(0, 1))
 
+## viability prop by bio by leaves present
+ggplot(brni.allo.phyto, aes(x=total.biomass.g.x, y=prop.viable)) +
+  geom_point() +
+  facet_wrap(~leaves.present)# +
+# geom_smooth(method = "lm")
+
+## viability prop by bkgrd
+ggplot(brni.allo.phyto, aes(x=bkgrd, y=prop.viable)) +
+  geom_point()
+
+ggplot(brni.allo.phyto, aes(x=total.biomass.g.x, y=prop.viable)) +
+  geom_point() +
+  facet_wrap(~bkgrd)
+
+## viability prop is really not related to any measured variables, try viable pod number
+    ## by bio and treatment
+ggplot(brni.allo.phyto, aes(x=total.biomass.g.x, y=viable.pod.num, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~leaves.present)
+    ## by just bio
+ggplot(brni.allo.phyto, aes(x=total.biomass.g.x, y=viable.pod.num)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+## looking better 
+
+## explore models ####
+viability.test <- lm(prop.viable ~ treatment*block*plot, data = brni_allo2)
+summary(viability.test)
+viability.test2 <- lm(prop.viable ~ total.flower.num, data = brni_allo2)
+summary(viability.test2)
+## borderline significant relationship with total.flower.num
+viability.test3 <- lm(prop.viable ~ pod.num, data = brni_allo2)
+summary(viability.test3)
+## not a signficant relationship with pod.num
+
+
+## tot via flowers by bio ####
+### calc tot via flowers ####
+viability_calcs <- brni.allo.phyto %>%
+  mutate(viable.flowers = flower.num*prop.viable,
+         total.viable.flowers = viable.flowers + viable.pod.num)
+
+### model ####
+## check viable pods by bio
+brni_via_lin <- lm(viable.pod.num~total.biomass.g, data = brni_allo2)
+summary(brni_via_lin)
+## R2 = 0.6602
+
+#### *tot via flowers by bio ####
+brni_via_lin3 <- lm(total.viable.flowers~total.biomass.g.x, data = viability_calcs)
+summary(brni_via_lin3)
+## R2 = 0.7875
+## use this relationship!
+
+### visualize ####
+ggplot(viability_calcs, aes(x=total.biomass.g.x, y=total.viable.flowers)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
+# Seeds/flower ####
+brni_mean_seeds_trt <- brni_seeds %>%
+  group_by(treatment) %>%
+  summarise(mean_seeds = mean(seeds.per.one.pod), se_seeds = calcSE(seeds.per.one.pod))
+
+brni_mean_seeds <- brni_seeds %>%
+  summarise(mean_seeds = mean(seeds.per.one.pod), se_seeds = calcSE(seeds.per.one.pod))
+
+ggplot(brni_mean_seeds_trt, aes(x=treatment, y=mean_seeds)) +
+  geom_point(size = 4) +
+  geom_errorbar(aes(ymin = mean_seeds - se_seeds, ymax = mean_seeds + se_seeds), width = 0.25)
+## interestingly, drought has higher average seeds/pod; some mild overlap of error bars
+
+seed.test <- aov(seeds.per.one.pod ~ treatment, data = brni_seeds)
+summary(seed.test)
+## no significant effect of treatment, use the same mean for both treatments
+
+
+
+# Save output ####
+## save the model outputs
+BRNI.allo.output <- data.frame(Species = "BRNI", 
+                               intercept = 0, 
+                               intercept_pval = NA, 
+                               intercept_se = NA, 
+                               
+                               slope = brni_allo_lin$coefficients[2], 
+                               slope_pval = summary(brni_allo_lin)$coefficients[2,4], 
+                               slope_se = summary(brni_allo_lin)$coefficients[2,2], 
+                               poly = NA, 
+                               poly_pval = NA, 
+                               poly_se = NA,
+                               
+                               seeds_C = brni_mean_seeds[,1],
+                               seeds_C_se = brni_mean_seeds[,2],
+                               seeds_D = brni_mean_seeds[,1],
+                               seeds_D_se = brni_mean_seeds[,2],
+                               
+                               viability_C = NA,
+                               viability_C_se = NA,
+                               viability_D = NA,
+                               viability_D_se = NA,
+                               
+                               viability_slope = brni_via_lin3$coefficients[2],
+                               viability_slope_pval = summary(brni_via_lin3)$coefficients[2,4],
+                               viability_slope_se = summary(brni_via_lin3)$coefficients[2,2])
+
+# Clean Env ####
+rm(list = c("brni.allo.phyto", "brni.phyto", "brni_allo", "brni_allo_lin", "brni_allo_lin_trt", "brni_allo_pol", "brni_allo2", "brni_mean_seeds", "brni_mean_seeds_trt", "brni_seeds",  "brni_via_lin", "brni_via_lin3", "lead", "seed.test", "viability.test", "viability.test2", "viability.test3", "viability_calcs"))
--- allometry/leni_allometry_testing.R
+++ allometry/leni_allometry_testing.R
@@ -1,6 +1,8 @@
+## LENI Allometry Testing
+
+## set up env
 library(tidyverse)
-library(ggpubr)
-library(openxlsx)
+theme_set(theme_bw())
 
 ## create a function to calculate standard error
 calcSE<-function(x){
@@ -9,60 +11,75 @@ calcSE<-function(x){
 }
 
 # Read in Data ####
-## Processing data
-#source("data_cleaning/merge_processing_collections_data.R")
-
-allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/" # Carmen's file path
-date <- 20230123
-
-## Allometry data
-leni_allo <- read.csv(paste0(allo_lead, "LENI_allometry-processing_", date, ".csv")) ## use when relationship is more finalized
-
-
-leni_no_dup <- leni_allo %>%
-  mutate(unique = toupper(unique)) %>%
-  group_by(block, plot, sub, unique, treatment) %>%
-  summarise(total.stem.length.mm.uni = unique(total.stem.length.mm),
-            pod.num.uni = unique(pod.num),
-            seed.num.uni = unique(seed.num))
-
-## save this and work from this
-write.csv(leni_no_dup, paste0(allo_lead, "LENI_allometry-processing_no-dup_20230123.csv"))
+## Allo Data ####
+# specify dropbox pathway 
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/")){
+  # Carmen
+  allo_lead <- "/Users/carme/Dropbox (University of Oregon)/Mega_Competition/Data/Allometry/Allometry_entered/"
+  
+} else {
+  # Marina
+  allo_lead <- "/Users/Marina/Documents/Dropbox/Mega_Competition/Data/Allometry/Allometry_entered/"
+} 
 
 
-# Dat Range ####
-theme_set(theme_bw())
+leni_allo <- read.csv(paste0(allo_lead, "LENI_allometry-processing_20230307.csv"))
 
-leni_dat <- all_dat_final %>%
-  filter(phyto == "LENI")
-
-phyto<-ggplot(leni_dat, aes(x=total.stem.length.mm.percap)) +
-  geom_histogram() +
-  facet_wrap(~treatment)
-
-allo<-ggplot(leni_allo, aes(x=total.stem.length.mm)) +
-  geom_histogram() +
-  facet_wrap(~treatment) +
-  coord_cartesian(xlim = c(0,6000))
+nrow(leni_allo[leni_allo$treatment == "D",]) ## 27
+nrow(leni_allo[leni_allo$treatment == "C",]) ## 21
+  ## uneven sample numbers as some relationships were sampled with a lot of samples of similar sizes initially
 
 
-ggarrange(phyto, allo, ncol = 1, nrow=2)
+# Visualize ####
+## linear ####
+ggplot(leni_allo, aes(x = total.biomass.g, y = pod.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, linewidth = 0.75, formula = y ~ x)
 
-ggsave("allometry/preliminary_figs/allometric_relationship_fits/leni_allometry_check.png", height = 4, width = 6)
-
-## To-Do ####
-    ## LENI relationship needs filling out everywhere, missing med-high values particularly
-
-
-
-
-
-
+## polynomial ####
+ggplot(leni_allo, aes(x = total.biomass.g, y = pod.num)) +
+  geom_point() +
+  geom_smooth(method = "lm", alpha = 0.25, linewidth = 0.75, formula = y ~ poly(x, 2))
 
 
+# Model ####
+## linear ####
+leni_allo_rel_lin <- lm(pod.num ~ total.biomass.g, data = leni_allo)
+summary(leni_allo_rel_lin) # r2 = 0.986
+
+## polynomial ####
+leni_allo_rel_pol <- lm(pod.num ~ total.biomass.g + I(total.biomass.g^2), data = leni_allo)
+summary(leni_allo_rel_pol) # r2 = 0.986
+## polynomial term is not significant and the model R2 is the same as the linear model. Might as well use linear and keep it simple.
 
 
+# Save Output ####
+LENI.allo.output <- data.frame(Species = "LENI", 
+                               intercept = 0, 
+                               intercept_pval = NA, 
+                               intercept_se = NA, 
+                               
+                               slope = leni_allo_rel_lin$coefficients[2], 
+                               slope_pval = summary(leni_allo_rel_lin)$coefficients[2,4], 
+                               slope_se = summary(leni_allo_rel_lin)$coefficients[2,2], 
+                               
+                               poly = NA, 
+                               poly_pval = NA, 
+                               poly_se = NA,
+                               
+                               seeds_C = NA,
+                               seeds_C_se = NA,
+                               seeds_D = NA,
+                               seeds_D_se = NA, 
+                               
+                               viability_C = NA,
+                               viability_C_se = NA,
+                               viability_D = NA,
+                               viability_D_se = NA,
+                               
+                               viability_slope = NA,
+                               viability_slope_pval = NA,
+                               viability_slope_se = NA)
 
-
-
-
+# Clean Env ####
+rm(list = c("allo_lead", "leni_allo", "leni_allo_rel_lin", "leni_allo_rel_pol"))
