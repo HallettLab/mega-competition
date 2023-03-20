db321d1e6a30a29c7dd8e2a1e2d9dbce14cfe396
ebel-c <cebel2@uoregon.edu>
2023-03-20 13:09:25 -0400

    add in LENI & BRNI

--- data_cleaning/phyto-processing_data-cleaning/compile_phyto-processing_data.R
+++ data_cleaning/phyto-processing_data-cleaning/compile_phyto-processing_data.R
@@ -9,9 +9,11 @@ source("data_cleaning/phyto-processing_data-cleaning/ANAR_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/AVBA_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/AMME_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/BRHO_phyto.R")
+source("data_cleaning/phyto-processing_data-cleaning/BRNI_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/CESO_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/CLPU_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/GITR_phyto.R")
+source("data_cleaning/phyto-processing_data-cleaning/LENI_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/LOMU_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/MAEL_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/MICA_phyto.R")
@@ -22,8 +24,8 @@ source("data_cleaning/phyto-processing_data-cleaning/THIR_phyto.R")
 source("data_cleaning/phyto-processing_data-cleaning/TWIL_phyto.R")
 
 ## merge all final phyto seeds in/seeds out dfs together
-all.phytos <- do.call("rbind", list(acam.phyto, anar.phyto, amme.phyto, avba.phyto, brho.phyto, ceso.phyto, clpu.phyto, gitr.phyto, lomu.phyto, mael.phyto, mica.phyto, pler.phyto, plno.phyto, taca.phyto, thir.phyto, twil.phyto))
+all.phytos <- do.call("rbind", list(acam.phyto, anar.phyto, amme.phyto, avba.phyto, brho.phyto, brni.phyto, ceso.phyto, clpu.phyto, gitr.phyto, leni.phyto, lomu.phyto, mael.phyto, mica.phyto, pler.phyto, plno.phyto, taca.phyto, thir.phyto, twil.phyto))
     ## add more phytos in as they are ready here.
 
 ## clean env 
-rm(list = c("acam.phyto", "anar.phyto", "amme.phyto", "avba.phyto", "basic_cleaning_func", "brho.phyto", "ceso.phyto",  "clpu.phyto", "gitr.phyto", "lomu.phyto", "mael.phyto", "mica.phyto", "pler.phyto", "plno.phyto", "taca.phyto", "thir.phyto", "twil.phyto", "not_planted", "i", "drought"))
+rm(list = c("acam.phyto", "anar.phyto", "amme.phyto", "avba.phyto", "basic_cleaning_func", "brho.phyto", "brni.phyto", "ceso.phyto",  "clpu.phyto", "gitr.phyto", "leni.phyto", "lomu.phyto", "mael.phyto", "mica.phyto", "pler.phyto", "plno.phyto", "taca.phyto", "thir.phyto", "twil.phyto", "not_planted", "i", "drought"))
