9d43cc9969f47dec2e391a603155c1f69c949400
Carmen Watkins <cebel2@uoregon.edu>
2023-12-04 17:56:24 -0800

    tweak data cleaning to use stems data for phytos NOT seeds in*germ

--- data_cleaning/format_model_dat.R
+++ data_cleaning/format_model_dat.R
@@ -9,7 +9,7 @@ source("data_cleaning/phyto-collections_data-cleaning/phyto-collections_data-cle
 ## an update from 9/1/2023 when determined should use stems data, NOT back calculated seeds in
 
 ## germination
-source("data_cleaning/germination_data-cleaning/germination_data_cleaning.R")
+#source("data_cleaning/germination_data-cleaning/germination_data_cleaning.R")
 
 all.phytos <- all.phytos %>%
   mutate(bkgrd = ifelse(bkgrd == "ERBO", "Control", bkgrd))
@@ -102,23 +102,22 @@ unique.id.controls <- sort(unique(repeated.controls$unique.ID))
 # Join Data ####
 ## join phyto & bkgrd data
 phyto.bg.dat <- left_join(with.controls, phyto.census, by = c("unique.ID")) %>%
-  select(-phyto.n.indiv.x, -phyto.n.indiv.y) %>%
+  select(-phyto.n.indiv.y) %>%
+  mutate(phyto.n.indiv = phyto.n.indiv.x) %>%
   ## if an observation is a control background, input 0 seeds
-  mutate(bg.stems = ifelse(unique.ID %in% unique.id.controls, 0, bkgrd.n.indiv))
+  mutate(bkgrd.n.indiv = ifelse(dens == "none", 0, bkgrd.n.indiv)) %>%
+  select(-phyto.n.indiv.x)
 
 ## check to see if the merge caused any errors (likely due to mismatch b/w phyto.n.indiv)
 na.bg <- phyto.bg.dat %>%
-  filter(is.na(bg.stems))
+  filter(is.na(bkgrd.n.indiv))
 ## all good!
-
-## reorder columns
-#bg.phyto.seeds <- bg.phyto.seeds[,c(1,5:8,11,9:10,2:4,12:13)] 
 
 # Replicate Info ####
 ## create df with number of reps for each precip-background-phyto species combo
 reps <- phyto.bg.dat %>%
   filter(dens != "none") %>%
-  mutate(bg.indiv.absent = ifelse(bg.stems == 0, 1, 0)) %>%
+  mutate(bg.indiv.absent = ifelse(bkgrd.n.indiv == 0, 1, 0)) %>%
   group_by(treatment, phyto, bkgrd) %>%
   summarise(bad.reps = sum(bg.indiv.absent), 
             tot.reps = n()) %>%
@@ -135,20 +134,19 @@ good.reps <- reps %>%
 good.reps.vec <- unique(good.reps$combos)
 
 # Format for Models ####
-model.dat.init <- phyto.bg.dat %>%
+model.dat <- phyto.bg.dat %>%
   mutate(bkgrd.names.in = bkgrd) %>%
-  pivot_wider(names_from = "bkgrd.names.in", values_from = "bg.stems", values_fill = 0) %>%
-  select(-bkgrd.n.indiv) %>% 
+  pivot_wider(names_from = "bkgrd.names.in", values_from = "bkgrd.n.indiv", values_fill = 0) %>%
+  #select(-bkgrd.n.indiv) %>% 
   mutate(combos = paste(phyto, bkgrd, treatment, sep = "_")) %>%
   filter(combos %in% good.reps.vec) %>% ## filter by rep #
   mutate(weeds = CRCO + ERBO + FIGA + GAMU + HYGL + SIGA + other, 
          trt = ifelse(treatment == "D", 0, 1)) %>% ## lump all the weeds together
-  select(-CRCO, -ERBO, -FIGA, -GAMU, -HYGL, -SIGA, -other)
+  select(-CRCO, -ERBO, -FIGA, -GAMU, -HYGL, -SIGA, -other, -phyto.seed.in)
 ## have decided to model intraspecific phytos as seeds out from the phytometer,
 ## NOT the seeds out from the phyto + estimated bg seeds out
 
-# Join Germ Data ####
-model.dat <- left_join(model.dat.init, mean.germ, by = c("phyto", "treatment"))
+write.csv(model.dat, "data/model_dat.csv")
 
 # Clean Env ####
 rm(all.phytos, allo.df, calcSE, collectionsC, i, lead, phyto.census, unique.key, with.controls, tmp.repeated.reps, tmp.controls, repeated.controls, bkgrd.df, all.blocks,  bkgrds, blocks, control.reps, j, k, tmp.block, tmp.rep, tmp.sp, all.reps, model.dat.init, bkgrd.n.indiv, mean.germ, na.bg, phyto.bg.dat, reps, good.reps)
