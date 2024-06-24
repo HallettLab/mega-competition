8007769923ab7371718d89979a99b735042f1a34
Carmen Watkins <cebel2@uoregon.edu>
2024-06-24 11:15:48 -0700

    try structural w/6 sp comm & random draws

---
+++ analyses/interactions_v_traits/structural_coexistence/structural_updated.R
@@ -0,0 +1,289 @@
+
+library(RcppAlgos)
+
+code_loc <- "analyses/interactions_v_traits/structural_coexistence/Saavedra_2017_code/"
+
+## load structural coexistence functions from Saavedra 2017
+source(paste0(code_loc, "toolbox_coexistence.R"))
+source(paste0(code_loc, "toolbox_figure.R"))
+
+## load posteriors
+source("analyses/interactions_v_traits/random_draws/clean_posteriors.R")
+
+# Prep DF ####
+
+## Ideal data format for this... matrix of interaction coeff? from one model run
+## need to select the same phyto species and background species 
+    ## for instance, GITR, PLER, MICA and alpha_gitr, alpha_pler, alpha_mica
+
+## need an if-else statement to handle NAs
+
+# Testing ####
+posts <- 1:3750
+
+draws <- sample(posts, 200, replace = FALSE)
+
+sp <- unique(posts_clean$species)
+
+
+## create all possible combinations of composition at a given richness level
+comp6 <- data.frame(comboGeneral(sp, m=6, freqs = 1))
+
+## create empty dataframe
+allcommC <- data.frame(ACAM = NA, AMME = NA, ANAR = NA, BRHO = NA, CESO=NA, GITR=NA, LOMU= NA, MAEL= NA, MICA= NA, PLER= NA, PLNO= NA, TACA= NA, THIR= NA, TWIL = NA, feasibility=NA, niche_diff = NA, fitness_diff = NA, draw = NA)
+
+## iterate over each possible community composition
+for(j in 1:nrow(comp6)){
+  
+  ## create a vector of community composition for each iteration
+  cc <- as.character(comp6[j,])
+  
+  ## filter data
+  prep <- posts_clean %>%
+    filter(species %in% cc) %>%
+    select(species, lambda_c, paste0("alpha_", tolower(cc[1]), "_c"), paste0("alpha_", tolower(cc[2]), "_c"), paste0("alpha_", tolower(cc[3]), "_c"), paste0("alpha_", tolower(cc[4]), "_c"), paste0("alpha_", tolower(cc[5]), "_c"), paste0("alpha_", tolower(cc[6]), "_c"))
+
+## iterate over posterior draws
+for (i in 1:length(draws)) {
+  
+  ## select particular model draw
+  d <- draws[i]
+  
+  ## select one row for every species
+  prep2 <- prep[c(d, (d + 3750), (d+(2*3750)), (d+(3*3750)), (d+(4*3750)), (d+(5*3750)), (d+(6*3750))),] 
+  
+  ## rename
+  names(prep2) <- c("sp", "lambda", cc[1], cc[2], cc[3], cc[4], cc[5], cc[6])
+  
+  ## remove row of NAs that keeps showing up
+  prep2 <- prep2 %>%
+    filter(!is.na(lambda))
+  
+  ## select the matching columns
+  tmp_alphas <- prep2 %>%
+    ungroup() %>%
+    select(all_of(cc)) %>%
+    as.matrix()
+  
+  ## pull out intrinsic growth rate vector
+  tmp_igr <- as.numeric(prep2$lambda)
+  
+  
+  if (any(is.na(tmp_alphas)) == FALSE) {
+    
+    ## test feasibility
+    f <- test_feasibility(alpha = tmp_alphas, r = tmp_igr)
+    
+    ## calculate structural niche differences
+    niche <- Omega(alpha=tmp_alphas)
+    
+    ## calculate structural fitness differences
+    fitness <- theta(alpha = tmp_alphas, r=tmp_igr) 
+    
+    
+  } else {
+    
+    f <- NA
+    niche <- NA
+    fitness <- NA
+    
+  }
+
+  
+  ## create dataframe
+  temp <- data.frame(feasibility=NA)
+  
+  ## put back into dataframe
+  temp$feasibility <- f
+  temp$niche_diff <- niche
+  temp$fitness_diff <- fitness
+
+## get comm composition back into dataframe
+temp <- temp %>%
+  mutate(ACAM = ifelse("ACAM" %in% colnames(tmp_alphas), 1, 0), 
+         AMME = ifelse("AMME" %in% colnames(tmp_alphas), 1, 0),
+         ANAR = ifelse("ANAR" %in% colnames(tmp_alphas), 1, 0),
+         BRHO = ifelse("BRHO" %in% colnames(tmp_alphas), 1, 0),
+         CESO = ifelse("CESO" %in% colnames(tmp_alphas), 1, 0), 
+         GITR = ifelse("GITR" %in% colnames(tmp_alphas), 1, 0), 
+         LOMU = ifelse("LOMU" %in% colnames(tmp_alphas), 1, 0),
+         MAEL = ifelse("MAEL" %in% colnames(tmp_alphas), 1, 0),
+         MICA = ifelse("MICA" %in% colnames(tmp_alphas), 1, 0),
+         PLER = ifelse("PLER" %in% colnames(tmp_alphas), 1, 0),
+         PLNO = ifelse("PLNO" %in% colnames(tmp_alphas), 1, 0), 
+         TACA = ifelse("TACA" %in% colnames(tmp_alphas), 1, 0),
+         THIR = ifelse("THIR" %in% colnames(tmp_alphas), 1, 0),
+         TWIL = ifelse("TWIL" %in% colnames(tmp_alphas), 1, 0), 
+         draw = d) ## add treatment column
+
+## join with original dataframe
+allcommC <- rbind(allcommC, temp)
+  
+}
+}
+
+allcomm <- allcomm[-1,]
+
+ggplot(allcomm, aes(x=feasibility)) +
+  geom_bar()
+
+ggplot(allcomm, aes(x=niche_diff)) +
+  geom_density()
+
+## error notes:  ####
+## Error in solve.default(t(alpha) %*% alpha) :
+  ## system is computationally singular: reciprocal condition number = 1.15444e-16
+
+## d = 1254; i = 133; j = 557
+
+## not sure if this error means it can't be calculated for this particular matrix?
+
+
+
+
+# Struct. Coexist Calc ####
+
+## make a vector of all unique species
+species <- unique(mean_alphas$species)
+
+## change column names to make the for loop easier to write
+colnames(mean_alphas) <- c("treatment", "species", "lambda", "ACAM", "AMME", "ANAR", "AVBA", "BRHO", "BRNI", "CESO", "CLPU", "GITR", "LENI", "LOMU", "MAEL", "MICA", "PLER", "PLNO", "TACA", "THIR", "TWIL")
+
+## create empty dataframe
+allcomm <- data.frame(ACAM = NA, AMME = NA, ANAR = NA, AVBA = NA, BRHO = NA, CESO=NA, GITR=NA, LOMU= NA, MAEL= NA, MICA= NA, PLER= NA, PLNO= NA, TACA= NA, THIR= NA, TWIL = NA, feasibility=NA, rich = NA, treatment = NA, niche_diff = NA, fitness_diff = NA, community = NA)
+
+## create comm richness level
+richness <- 2:18
+
+## create vector of treatments
+treat <- unique(mean_alphas$treatment)
+
+
+## iterate calcs over every comm comp and treatment
+for(k in 1:length(treat)){
+  
+  
+  ## iterate at every richness level
+  for(i in 1:length(richness)){
+    
+    ## create all possible combinations of composition at a given richness level
+    comp <- data.frame(comboGeneral(species, m=richness[i], freqs = 1))
+    
+    
+    ## iterate over each possible community composition
+    for(j in 1:nrow(comp)){
+      
+      ## create a vector of community composition for each iteration
+      cc <- as.character(comp[j,])
+      
+      trt <- treat[k]
+      
+      ## select precipitation treatment
+      ## randomly sample rows
+      tmp <- mean_alphas %>%
+        filter(treatment %in% trt) %>%
+        filter(species %in% cc)
+      
+      sp <- tmp$species
+      
+      ## select the matching columns
+      tmp_alphas <- tmp %>%
+        ungroup() %>%
+        select(all_of(sp)) %>%
+        as.matrix()
+      
+      ## pull out intrinsic growth rate vector
+      tmp_igr <- as.numeric(tmp$lambda)
+      
+      ## test feasibility
+      f <- test_feasibility(alpha = tmp_alphas, r = tmp_igr)
+      
+      ## calculate structural niche differences
+      niche <- Omega(alpha=tmp_alphas)
+      
+      ## calculate structural fitness differences
+      fitness <- theta(alpha = tmp_alphas, r=tmp_igr) 
+      
+      ## create dataframe
+      temp <- data.frame(feasibility=NA)
+      
+      ## put back into dataframe
+      temp$feasibility <- f
+      temp$niche_diff <- niche
+      temp$fitness_diff <- fitness
+      
+      ## get comm composition back into dataframe
+      temp <- temp %>%
+        mutate(ACAM = ifelse("ACAM" %in% colnames(tmp_alphas), 1, 0), 
+               AMME = ifelse("AMME" %in% colnames(tmp_alphas), 1, 0),
+               ANAR = ifelse("ANAR" %in% colnames(tmp_alphas), 1, 0),
+               AVBA = ifelse("AVBA" %in% colnames(tmp_alphas), 1, 0),
+               BRHO = ifelse("BRHO" %in% colnames(tmp_alphas), 1, 0),
+               CESO = ifelse("CESO" %in% colnames(tmp_alphas), 1, 0), 
+               GITR = ifelse("GITR" %in% colnames(tmp_alphas), 1, 0), 
+               LOMU = ifelse("LOMU" %in% colnames(tmp_alphas), 1, 0),
+               MAEL = ifelse("MAEL" %in% colnames(tmp_alphas), 1, 0),
+               MICA = ifelse("MICA" %in% colnames(tmp_alphas), 1, 0),
+               PLER = ifelse("PLER" %in% colnames(tmp_alphas), 1, 0),
+               PLNO = ifelse("PLNO" %in% colnames(tmp_alphas), 1, 0), 
+               TACA = ifelse("TACA" %in% colnames(tmp_alphas), 1, 0),
+               THIR = ifelse("THIR" %in% colnames(tmp_alphas), 1, 0),
+               TWIL = ifelse("TWIL" %in% colnames(tmp_alphas), 1, 0), 
+               rich = richness[i], ## add richness column
+               treatment = trt, 
+               community = paste(i,j, sep = "")) ## add treatment column
+      
+      ## join with original dataframe
+      allcomm <- rbind(allcomm, temp)
+      
+    }
+    
+  }
+  
+}
+
+#all_comm <- allcomm[-1,]
+#write.csv(all_comm, "models/CW/structural_coexistence_output.csv")
+
+# Visualize ####
+## Trt Facet ####
+## faceted by treatment, colored by richness, and shape by feasibility 
+ggplot(all_comm, aes(x=exp(niche_diff), y=fitness_diff, color = as.factor(rich), shape = as.factor(feasibility))) +
+  geom_point() +
+  #theme_few() +
+  facet_wrap(~treatment) +
+  #coord_cartesian(xlim = c(0,1)) +
+  #scale_color_manual(values = vivid) +
+  scale_shape_manual(values = c(1,16)) +
+  #xlab("Structural Niche Differences") + ylab("Structural Fitness Differences") + labs(col = "Richness", shape = "Feasibility") +
+  geom_abline(aes(intercept = 0, slope = 45)) + ## boundary of predicted coexistence or competitive exclusion
+  theme(text = element_text(size = 14)) +
+  theme_bw()
+
+#ggsave("structural_coexistence.png", height = 5, width = 8)
+
+
+## Rich-Trt Facet ####
+ggplot(all_comm %>%
+         arrange(feasibility), aes(x=exp(niche_diff), y=fitness_diff, shape = as.factor(feasibility), color = as.factor(feasibility))) +
+  geom_point() +
+  #theme_few() +
+  facet_wrap(~as.factor(rich)*treatment, ncol = 8) +
+  #coord_cartesian(xlim = c(0,1)) +
+  scale_color_manual(values = c("gray", "black")) +
+  scale_shape_manual(values = c(1,16)) +
+  #xlab("Structural Niche Differences") + ylab("Structural Fitness Differences") + labs(col = "Richness", shape = "Feasibility") +
+  geom_abline(aes(intercept = 0, slope = 45)) + ## boundary of predicted coexistence or competitive exclusion
+  theme(text = element_text(size = 14)) +
+  theme_bw()
+#ggsave("structural_coexistence_rich_trt_updated.png", height = 10, width = 12)
+
+
+## F Comm ONLY ####
+## looking ONLY at feasible communities
+ggplot(f_comm, aes(x=exp(niche_diff), y=fitness_diff, shape = as.factor(treatment), color = as.factor(rich))) +
+  geom_point() +
+  geom_abline(aes(intercept = 0, slope = 45)) + ## boundary of predicted coexistence or competitive exclusion
+  scale_shape_manual(values = c(1,16)) +
+  theme_bw()
+#ggsave("structural_coexistence_feasible_comm_trt.png", height = 3, width = 5)
