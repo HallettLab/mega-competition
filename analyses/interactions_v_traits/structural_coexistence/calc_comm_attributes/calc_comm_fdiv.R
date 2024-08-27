535d21f305560892a1e8ad3891311204d5ec84fc
Carmen Watkins <cebel2@uoregon.edu>
2024-08-27 09:02:28 -0700

    calc fdiv for every comp at every richness level

--- analyses/interactions_v_traits/structural_coexistence/calc_comm_attributes/calc_comm_fdiv.R
+++ analyses/interactions_v_traits/structural_coexistence/calc_comm_attributes/calc_comm_fdiv.R
@@ -1,8 +1,10 @@
 ## Calc community functional trait diversity
+## for all possible community compositions at richness levels 4-16
 
 # Set up ####
 library(tidyverse)
 library(mFD)
+library(RcppAlgos)
 
 calcSE<-function(x){
   x2<-na.omit(x)
@@ -16,13 +18,7 @@ source("analyses/traits/clean_trait_data.R")
 ## following instructions from this https://cmlmagneville.github.io/mFD/articles/Continuous_traits_framework.html#load-dataset to calculate functional diversity. 
 ## associated with paper: Magneville et al. 2022; Ecography
 
-## summarise trait & assemblage data
-## calculate trait based distances b/w species pairs
-## compute multidimensional functional space
-
-## data taken from trait pcas exploration script
-
-# Clean Trait Data ####
+# Prep Trait Data ####
 ## select traits
 all.traits <- c("Height_cm", "LDMC", "SLA.cm2.g", "RMF",  "Coarse.root.specific.length.cm.g", "Proportion.fine.roots", "Coarse.root.diameter.mm")
 
@@ -65,11 +61,8 @@ sp_dist = funct.dist(sp_tr = t_mat,
                      tr_cat = trait_mdat,
                      metric = "euclidean")
 
-
-
-
 ## calculate functional space - not quite sure what this step is?
-fspace = tr.cont.fspace(sp_tr = test,
+fspace = tr.cont.fspace(sp_tr = t_mat,
                         pca = TRUE,
                         nb_dim = 7)
 
@@ -81,49 +74,90 @@ fspace$"eigenvalues_percentage_var"
 dist_mat_pca <- as.matrix(fspace$sp_dist_multidim$"6D")
 dist_mat_trait <- as.matrix(fspace$sp_dist_init)
 
-## select distinct community compositions
-comms = allcomm[,c(1:16, 24)] %>%
-  distinct()
+## Select community comps ####
+## create a vector of all species
+all.sp = unique(MC.pca.ID$phyto)
 
+## create a vector of all richness levels
+richness = c(4:16)
 
-natcomm2 = natcomm%>%
-  mutate(comp = paste0(ACAM, AMME, GITR, LENI, MAEL, MICA, PLER, PLNO, TWIL))
+## create an empty array? 
+comp_lists = list()
 
-comms = natcomm2[,c(1:9, 19)] %>%
-  filter(!is.na(GITR)) %>%
-  distinct()
+## loop through all richness levels & create df with all combinations of possible community compositions
+for(r in 1:length(richness)) {
+  
+  ## select a richness level
+  rich = richness[r]
+  
+  ## calculate all combos of composition
+  comp <- data.frame(comboGeneral(all.sp, m=rich, freqs = 1)) %>% 
+    rowwise() %>%
+    mutate(comp_list = list(c_across(starts_with("X")))) %>%
+    mutate(ACAM = ifelse("ACAM" %in% comp_list, 1, 0), ## change to 1's and 0's for P/A
+           AMME = ifelse("AMME" %in% comp_list, 1, 0),
+           ANAR = ifelse("ANAR" %in% comp_list, 1, 0),
+           BRHO = ifelse("BRHO" %in% comp_list, 1, 0),
+           BRNI = ifelse("BRNI" %in% comp_list, 1, 0),
+           CESO = ifelse("CESO" %in% comp_list, 1, 0),
+           GITR = ifelse("GITR" %in% comp_list, 1, 0),
+           LENI = ifelse("LENI" %in% comp_list, 1, 0),
+           LOMU = ifelse("LOMU" %in% comp_list, 1, 0),
+           MAEL = ifelse("MAEL" %in% comp_list, 1, 0),
+           MICA = ifelse("MICA" %in% comp_list, 1, 0),
+           PLER = ifelse("PLER" %in% comp_list, 1, 0),
+           PLNO = ifelse("PLNO" %in% comp_list, 1, 0),
+           TACA = ifelse("TACA" %in% comp_list, 1, 0),
+           THIR = ifelse("THIR" %in% comp_list, 1, 0),
+           TWIL = ifelse("TWIL" %in% comp_list, 1, 0),
+           comp = paste0(ANAR, BRHO, BRNI, CESO, LOMU, TACA, THIR, ACAM, AMME, GITR, LENI, MAEL, MICA, PLER, PLNO, TWIL),
+           richness = rich) %>%
+    select(ANAR, BRHO, BRNI, CESO, LOMU, TACA, THIR, ACAM, AMME, GITR, LENI, MAEL, MICA, PLER, PLNO, TWIL, comp, richness)
+  
+  ## append to list
+  comp_lists[[r]] = comp
+  
+}
 
-## get names of compositions to save as rownames
-cn = comms$comp
+## Create Matrix & Calc ####
+## create empty list
+fdiv_list = list()
 
-## create a matrix of P/A data with rownames of diff compositions
-commsmat = as.matrix(comms[1:16])
+## iterate thru each richness level & calc FD
+for(r in 1:length(richness)) {
+  
+  ## select a richness level
+  rich = richness[r]
+  
+  ## get names of compositions to save as rownames
+  cn = comp_lists[[r]]$comp
+  
+  ## create a matrix of P/A data with rownames of diff compositions
+  commsmat = as.matrix(comp_lists[[r]][1:16])
+  
+  ## save rownames
+  rownames(commsmat) = cn
+  
+  ## run final functional diversity calculation for each community
+  comms_fdiv = alpha.fd.hill(
+    asb_sp_w = commsmat,
+    sp_dist = dist_mat_trait,
+    q=0, ## defines relative importance given to species weights compared to species distances; increasing q gives increasing importance to species weights rather than trait-based distances
+    tau = "mean"  ## defines threshold level applied to functional distances b/w species to determine functionally distinct sets of species
+    ## minimum would yield taxonomic div; maximum & q=2 would yield Rao's Q
+  )
+  
+  ## extract functional div metric
+  fdiv = comms_fdiv$asb_FD_Hill
 
-commsmat = as.matrix(comms[1:9])
-rownames(commsmat) = cn
+  ## save into data frame
+  t_comms = as.data.frame(comp_lists[[r]])
+  t_comms$fdiv = fdiv
 
-## run final functional diversity calculation for each community
-comms_fdiv = alpha.fd.hill(
-  asb_sp_w = commsmat,
-  sp_dist = dist_mat_trait,
-  q=0, ## defines relative importance given to species weights compared to species distances; increasing q gives increasing importance to species weights rather than trait-based distances
-  tau = "mean"  ## defines threshold level applied to functional distances b/w species to determine functionally distinct sets of species
-  ## minimum would yield taxonomic div; maximum & q=2 would yield Rao's Q
-)
-
-## extract functional div metric
-fdiv = comms_fdiv$asb_FD_Hill
-
-## view
-hist(fdiv)
-
-## save into data frame
-comms$fdiv = fdiv
-
-## join with structural output
-allcomm_fdiv = left_join(allcomm, comms[, 17:18], by = "comp")
-allcomm_fdiv = left_join(natcomm2, comms[, 10:11], by = "comp")
-
+  ## append to list
+  fdiv_list[[r]] = t_comms
+  
+}
 
 # Clean Env ####
 rm(allcomm, comms, comms_fdiv, commsmat, dist_mat_pca, dist_mat_trait, fdiv, fspace, MC.pca.ID, MC.traits2, seed.sums, test, trait_mdat, trait_sum, trait_sums, traits, all.traits, cn, sp_dist)
