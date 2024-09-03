21c33371334560512806457de9a3b89461be1c74
Carmen Watkins <cebel2@uoregon.edu>
2024-09-03 09:17:32 -0700

    4 and 12 sp structural results

---
+++ analyses/interactions_v_traits/structural_coexistence/rough_model_progress.R
@@ -0,0 +1,21 @@
+test = read.csv("analyses/interactions_v_traits/structural_coexistence/4sp_structural_indirect_not_finished.csv")
+
+
+
+116406/200
+582/2
+291/1820
+
+
+116406/10106
+## rows per kb
+
+10106*11.5
+
+
+24436*11.5
+
+281014/100
+2810/2
+
+1405/1820
